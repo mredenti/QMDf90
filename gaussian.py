d80cea83c54e7964e752c854c7d9d0ba220d5d8c
Michael <mir00021@hotmail.com>
2022-06-06 12:51:19 +0100

    hello world

--- gaussian.py
+++ gaussian.py
@@ -2,18 +2,26 @@ import numpy as np
 
 class Gaussian():
 
-    def __init__(self, q, p, Q, P, eps, s=0, state=None):
+    def __init__(self, a, q, p, C, eps, s=0, state=None):
 
         self.q = q
         self.p = p
-        self.Q = Q
-        self.P = P
+        self.C = C
         self.eps = eps
         self.q = q
         self.s = s 
+        self.dim =1 
         self.state = state
+        self.a = a
 
     def psi(self, x):
 
-        return (np.pi * self.eps)**(-0.25) * self.Q**(-0.5) * np.exp(1j / 2 / self.eps * self.P / self.Q * (x - self.q)**2 + 1j*self.p*(x-self.q))
+        detC = np.linalg.det(self.C.imag) if self.dim != 1 else self.C.imag
 
+        return np.exp(1j/self.eps * self.s.real) * self.a * (np.pi * self.eps)**(-self.dim / 4) * detC**(0.25) * np.exp(1j / 2 / self.eps * (x - self.q).T * self.C * (x - self.q) + 1j / self.eps*self.p.T*(x-self.q))
+
+    def l2norm(self):
+
+        x, dx = np.linspace(self.q - 4, self.q + 4, 10**5, retstep=True)
+
+        return np.sum(np.abs(self.psi(x))**2) * dx  
